17c0f238f380d22690d1d4f882e8c6b9f5b53b66
Eao <caozhx7@mail2.sysu.edu.cn>
2024-06-04 09:51:03 +0800

    fix bug: invalid username or password return 401

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
